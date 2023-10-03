18cf970a4f9f534d6eaf78b5e045c173ff12f2e9
Brahim Nedjar <skullneed@gmail.com>
2023-10-03 23:23:54 +0100

    Initiated clean architecture and added image assets and colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgotPassword
+  /// assets/images/forgotPassword.png
+  static const String assetsImagesForgotPassword = "assets/images/forgotPassword.png";
+
+  /// Assets for assetsImagesOnBoarding
+  /// assets/images/onBoarding.png
+  static const String assetsImagesOnBoarding = "assets/images/onBoarding.png";
+
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String assetsImagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String assetsImagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for assetsImagesSucccesOrder
+  /// assets/images/succcesOrder.png
+  static const String assetsImagesSucccesOrder = "assets/images/succcesOrder.png";
+
+  /// Assets for assetsImagesVector1
+  /// assets/images/vector1.svg
+  static const String assetsImagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/vector2.svg
+  static const String assetsImagesVector2 = "assets/images/vector2.svg";
+}
